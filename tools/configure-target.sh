9d2cc0191345882fea19cc775ba546efbe182fbf
Rene Barto <rene.barto66@gmail.com>
2023-12-09 14:23:31 +0100

    Update scripts for target build

--- tools/configure-target.sh
+++ tools/configure-target.sh
@@ -2,6 +2,6 @@ rm -rf cmake-build/
 mkdir cmake-build
 pushd cmake-build
 
-cmake ../tutorial/01-building -G Ninja
+cmake ../tutorial/01-building -G Ninja -DCMAKE_BUILD_TYPE:STRING="Debug" -DCMAKE_TOOLCHAIN_FILE:FILEPATH=../tutorial/01-building/baremetal.toolchain
 
 popd

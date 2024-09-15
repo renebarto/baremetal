bd75caede47a2442cdef9f45d18ae70c1a379d36
Rene Barto <rene.barto66@gmail.com>
2024-09-15 19:11:35 +0200

    Update and configure build scripts to support both RPI3 and 4

--- tools/configure-rpi4.sh
+++ tools/configure-rpi4.sh
@@ -1,6 +1,6 @@
-rm -rf cmake-build/
-mkdir cmake-build
-pushd cmake-build
+rm -rf cmake-build-rpi4/
+mkdir cmake-build-rpi4
+pushd cmake-build-rpi4
 
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE:STRING="Debug" -DBAREMETAL_TARGET=RPI4 -DCMAKE_TOOLCHAIN_FILE:FILEPATH=../baremetal.toolchain
 
